2e2079a1ceb2bec0ed2d8058fef50d8a75631d9e
Mehdi <mah.fat@gmail.com>
2023-05-10 15:26:50 +0330

    Update clicode.py

--- clicode.py
+++ clicode.py
@@ -1,15 +1,21 @@
-def calculate_position_size(self):
-    account_balance = float(self.account_balance_field.text())
-    risk_pct_str = self.risk_pct_field.text()
-    if risk_pct_str.endswith('%'):
-        risk_pct_str = risk_pct_str[:-1]
-    risk_pct = float(risk_pct_str) / 100
-    entry_price = float(self.entry_price_field.text())
-    exit_price = float(self.exit_price_field.text())
-    stop_loss_price = float(self.stop_loss_price_field.text())
+# Import decimal module for precision arithmetic
+from decimal import Decimal, ROUND_HALF_UP
 
-    risk_amt = account_balance * risk_pct
-    risk_per_share = entry_price - stop_loss_price
-    position_size = round(risk_amt / risk_per_share, 2)
+# Ask for user input
+balance = Decimal(input("Enter account balance: "))
+risk_percent = Decimal(input("Enter risk percentage (as percentage): ")) / 100
+entry_price = Decimal(input("Enter entry price: "))
+exit_price = Decimal(input("Enter exit price: "))
+stop_loss_price = Decimal(input("Enter stop loss price: "))
 
-    self.result_label.setText(f"Position size: {position_size}")
+# Calculate position size
+risk_amount = balance * risk_percent
+risk_per_share = entry_price - stop_loss_price
+position_size = risk_amount / risk_per_share
+
+# Round position size to two decimal places
+position_size = position_size.quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)
+
+# Print result
+print("Position size:", position_size)
+ 
